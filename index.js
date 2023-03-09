f618699df9b672dd7dbcb838aabbd0dc2cf9cb44
NezMat <114604346+nenifaith@users.noreply.github.com>
2023-03-09 17:33:24 +0000

    we did some testing

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-var http = require("http");
-http.createServer(function(req,res) {
+var Calculator = require("./Calculator.js");//.js is optional
+var calc = new Calculator();
+let result = calc.divide(2,3);
+console.log(result);
 
-  var path = req.url.toLowerCase();
 
-  switch(path){
-    case "/":
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Home Page');
-      break; 
-          case "/about":
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('About Page');
-      break; 
 
-    default:
-       res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Not Found');
-        
-  }
-
-}).listen(process.env.PORT || 3000);
---
+++ test/Calculator.js
@@ -0,0 +1,42 @@
+//load node.js assertion library
+const chai = require("chai");
+const expect = chai.expect;
+const asserttype = require('chai-asserttype');
+chai.use(asserttype);
+
+
+//load calculator code
+var Calculator = require("../Calculator");
+var calc = new Calculator();
+
+describe("Calculator Tests",function(){
+ 
+  it("add: Adds 1 and 2",function(){
+  expect(calc.add(1,2)).to.be.equal(3);
+  });
+  it("Add Allows one parameter",function(){
+   expect(calc.add(1)).to.be.equal(1);
+ });
+ it("Add Allows no parameters",function(){
+   expect(calc.add()).to.be.equal(0);
+ });
+ it("Add turns strings to zeros",function(){
+   expect(calc.add("frog","toad")).to.be.number();
+ });
+
+
+  it("subtract: 5 from 10",function(){
+  expect(calc.subtract(10,5)).to.be.equal(5);
+  });
+   it("subtract allows one parameter",function(){
+  expect(calc.subtract(3)).to.be.equal(3);
+  });
+  
+  it("multiply: 5 times two",function(){
+  expect(calc.multiply(5,2)).to.be.equal(10);
+  });
+
+   it("divide: six divided by three",function(){
+    expect(calc.divide(6,3)).to.be.equal(2);
+  });
+});
