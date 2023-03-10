3bb1d5e4bcb06f61190ee04ad62b4d0f22fcef3f
NezMat <114604346+nenifaith@users.noreply.github.com>
2023-03-10 23:19:23 +0000

    I added some function

--- test/Calculator.js
+++ test/Calculator.js
@@ -14,23 +14,15 @@ describe("Calculator Tests",function(){
   it("add: Adds 1 and 2",function(){
   expect(calc.add(1,2)).to.be.equal(3);
   });
-  it("Add Allows one parameter",function(){
-   expect(calc.add(1)).to.be.equal(1);
+   it("Add Allows no parameters",function(){
+   expect(calc.subtract(3)).to.be.equal(3);
  });
- it("Add Allows no parameters",function(){
+    it("Add Allows one parameter",function(){
    expect(calc.add()).to.be.equal(0);
  });
- it("Add turns strings to zeros",function(){
+  it("Add turns strings to zeros",function(){
    expect(calc.add("frog","toad")).to.be.number();
  });
-
-
-  it("subtract: 5 from 10",function(){
-  expect(calc.subtract(10,5)).to.be.equal(5);
-  });
-   it("subtract allows one parameter",function(){
-  expect(calc.subtract(3)).to.be.equal(3);
-  });
   
   it("multiply: 5 times two",function(){
   expect(calc.multiply(5,2)).to.be.equal(10);
